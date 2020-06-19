d20a0571c21b344e3b87e008c1120abcc3c22f08
konovalov <ikonovalov@7tech.ru>
2020-06-19 22:35:50 +0300

    add tender

--- accelerator/models/tender.py
+++ accelerator/models/tender.py
@@ -3,4 +3,28 @@ from django.db import models
 
 class Tender(models.Model):
     """Идея/предложение, вынесенное на обсуждение."""
-    pass
+    caption = models.CharField(
+        max_length=128,
+        verbose_name='Наименование',
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+    )
+    direction = models.CharField(
+        max_length=16,
+        choices=[],
+        verbose_name='Направление/Категория',
+    )
+    presentation = models.FileField(
+        verbose_name='Файлы презентации',
+    )
+    status = models.CharField(
+        max_length=16,
+        choices=[],
+        verbose_name='Статус',
+        default='draft',
+    )
+    owner = models.ForeignKey(
+        on_delete=SET_NULL,
+
+    )
