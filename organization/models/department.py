014b004bc3ddabbe2332f119b2197e18de5836fc
konovalov <ikonovalov@7tech.ru>
2020-06-19 22:21:28 +0300

    refactor position, department migrations

--- organization/models/department.py
+++ organization/models/department.py
@@ -3,5 +3,16 @@ from django.db import models
 
 class Department(models.Model):
     """Структура организации."""
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
-    name = models.CharField('Наименование', max_length=512)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    name = models.CharField(
+        verbose_name='Наименование подразделения',
+        max_length=256,
+    )
+
+    def __str__(self):
+        return self.name
+
