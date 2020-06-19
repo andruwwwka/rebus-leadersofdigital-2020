e120ab3687b91fe97ec380edf02c17e29106ee00
konovalov <ikonovalov@7tech.ru>
2020-06-19 21:03:20 +0300

    add profile model

--- users/models/profile.py
+++ users/models/profile.py
@@ -1,17 +1,40 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from organization.models import Department
 from . import Position
-from ..organization.models import Department
 
 
 class Profile(AbstractUser):
     """Профиль пользователя."""
+    username = None
+    email = models.EmailField(
+        verbose_name='Емайл',
+        unique=True
+    )
+    patronymic = models.CharField(
+        max_length=128,
+        verbose_name='Отчество',
+    )
     department = models.ForeignKey(
         Department,
-        on_delete=models.SET_NULL
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='Департамент',
     )
     position = models.ForeignKey(
         Position,
-        on_delete=models.SET_NULL
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name='Должность',
     )
+    phone = models.EmailField(
+        max_length=32,
+        verbose_name='Телефон',
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['department', 'position']
+
+    def __str__(self):
+        return self.email
