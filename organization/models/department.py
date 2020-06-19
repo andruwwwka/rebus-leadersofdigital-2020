11eca25b9485310492cfcc9631eccebe9f110d15
konovalov <ikonovalov@7tech.ru>
2020-06-19 21:37:40 +0300

    add position, department

--- organization/models/department.py
+++ organization/models/department.py
@@ -4,3 +4,4 @@ from django.db import models
 class Department(models.Model):
     """Структура организации."""
     parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    name = models.CharField('Наименование', max_length=512)
--- users/models/position.py
+++ users/models/position.py
@@ -1,6 +1,18 @@
 from django.db import models
 
+POST_CHOICES = [
+    ('head', 'Руководитель'),
+    ('alternative_head', 'Заместитель руководителя'),
+    ('senior', 'Ведущий эксперт'),
+    ('middle', 'Эксперт'),
+    ('junior', 'Младший эксперт'),
+]
+
 
 class Position(models.Model):
     """Должности сотрудников."""
-    pass
+    post = models.CharField(
+        max_length=128,
+        choices=POST_CHOICES,
+        default='junior'
+    )
