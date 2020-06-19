7abfa9e4d4108fe7409d567024fe792265bc7e78
Andrey Leontyev <andruwwwka@gmail.com>
2020-06-20 02:49:55 +0300

    Feature/authorization (#5)
    * Менеджер для профиля
    
    * Авторизация ползователя
    
    * Фикс ошибки в сваггере

--- users/resources/login.py
+++ users/resources/login.py
@@ -1,8 +1,40 @@
-from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class LoginSerializer(serializers.Serializer):
+    """Сериалайзер для валидации параметров авторизации."""
+    email = serializers.EmailField(write_only=True, required=True)
+    password = serializers.CharField(max_length=128, write_only=True, required=True)
+
+    def validate(self, data):
+        """Валидация авторизационных параметров."""
+        email = data.get('email', None)
+        password = data.get('password', None)
+        user = authenticate(username=email, password=password)
+        if user is None:
+            raise serializers.ValidationError(
+                'Пользователь с таким email или паролем не найден.'
+            )
+        if not user.is_active:
+            raise serializers.ValidationError(
+                'Пользователь деактивирован.'
+            )
+        return {
+            'email': user.email,
+        }
 
 
 class Login(APIView):
+    """Авторизация пользователя."""
+
+    permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
 
     def post(self, request):
-        return Response({'alive': True})
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data)
