fc0826852c7a13a7e09e4676cd3013444aee7004
Andrey Leontyev <andruwwwka@gmail.com>
2020-06-20 05:52:49 +0000

    Удалена неиспользуемая мидлвара

--- leadersofdigital/middleware.py
+++
@@ -1,9 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-
-class CorsMiddleware(MiddlewareMixin):
-
-    def process_response(self, req, resp):
-        resp['Access-Control-Allow-Origin'] = '*'
-        resp['Access-Control-Allow-Headers'] = '*'
-        return resp
