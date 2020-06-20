ff11c5a5a71592efa0259aef883edd4b7c0c2dad
Andrey Leontyev <andruwwwka@gmail.com>
2020-06-20 16:00:47 +0300

    Методы API работы с командой (#15)
    * add draft team-rest
    
    * add team-rest v3
    
    * add team-rest v4
    
    Co-authored-by: konovalov <ikonovalov@7tech.ru>

--- accelerator/urls.py
+++ accelerator/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
+from .resources.team import TeamViewSet
 from .resources.tender import TenderViewSet
 
 router = DefaultRouter()
 router.register(r'tenders', TenderViewSet, basename='tenders')
+router.register(r'teams', TeamViewSet, basename='teams')
 urlpatterns = router.urls
